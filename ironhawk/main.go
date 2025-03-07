02384183b45d7f7699cd388c137434fb3a687e8b
aasifkhan7 <aasifk106@gmail.com>
2025-03-07 16:58:17 +0530

    Update main.go

--- ironhawk/main.go
+++ ironhawk/main.go
@@ -20,155 +20,192 @@ const (
     WatchMode   Mode = "watch"
 )
 
-var (
-    boldGreen = color.New(color.FgGreen, color.Bold).SprintFunc()
-    boldRed   = color.New(color.FgRed, color.Bold).SprintFunc()
-    boldBlue  = color.New(color.FgBlue, color.Bold).SprintFunc()
-    currentMode = CommandMode
-)
+type CLI struct {
+	client      *dicedb.Client
+	rl          *readline.Instance
+	mode        Mode
+	boldGreen   func(a ...interface{}) string
+	boldRed     func(a ...interface{}) string
+	boldBlue    func(a ...interface{}) string
+	sigChan     chan os.Signal
+	watchSigChan chan os.Signal
+}
 
-func Run(host string, port int) {
+func NewCLI(host string, port int) (*CLI, error) {
 	client, err := dicedb.NewClient(host, port)
 	if err != nil {
-		log.Fatal(err)
+		return nil, fmt.Errorf("failed to connect to database: %v", err)
 	}
-	defer client.Close()
 
 	rl, err := readline.NewEx(&readline.Config{
-		Prompt:      fmt.Sprintf("%s:%s> ", boldBlue(host), boldBlue(port)),
+		Prompt:      fmt.Sprintf("%s:%s> ", color.BlueString(host), color.BlueString(fmt.Sprint(port))),
 		HistoryFile: os.ExpandEnv("$HOME/.dicedb_history"),
 	})
 	if err != nil {
-		fmt.Printf("%s failed to initialize readline: %v\n", boldRed("ERR"), err)
-		return
+		return nil, fmt.Errorf("failed to initialize readline: %v", err)
 	}
-	defer rl.Close()
 
-	// Setup signal handling for main CLI
-	sigChan := make(chan os.Signal, 1)
-	signal.Notify(sigChan, os.Interrupt)
+	return &CLI{
+		client:      client,
+		rl:          rl,
+		mode:        CommandMode,
+		boldGreen:   color.New(color.FgGreen, color.Bold).SprintFunc(),
+		boldRed:     color.New(color.FgRed, color.Bold).SprintFunc(),
+		boldBlue:    color.New(color.FgBlue, color.Bold).SprintFunc(),
+		sigChan:     make(chan os.Signal, 1),
+		watchSigChan: make(chan os.Signal, 1),
+	}, nil
+}
 
-	// Handle Ctrl+C in a separate goroutine for the main CLI
+func Run(host string, port int) {
+	cli, err := NewCLI(host, port)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer cli.cleanup()
+
+	cli.setupSignalHandler()
+	cli.run()
+}
+
+func (c *CLI) cleanup() {
+	c.client.Close()
+	c.rl.Close()
+}
+
+func (c *CLI) setupSignalHandler() {
+	signal.Notify(c.sigChan, os.Interrupt)
 	go func() {
-		for range sigChan {
-			if currentMode == CommandMode {
-				fmt.Println("Received interrupt. Exiting...")
+		for range c.sigChan {
+			if c.mode == CommandMode {
+				fmt.Println("\nReceived interrupt. Exiting...")
 				os.Exit(0)
 			}
 		}
 	}()
+}
 
+func (c *CLI) run() {
 	for {
-		input, err := rl.Readline()
-		if err != nil { // io.EOF, readline.ErrInterrupt
+		input, err := c.rl.Readline()
+		if err != nil {
 			break
 		}
-		input = strings.TrimSpace(input)
 
-		if input == "exit" {
+		if !c.handleInput(strings.TrimSpace(input)) {
 			return
-		}
-
-		if input == "" {
-			continue
-		}
-
-		args := parseArgs(input)
-		if len(args) == 0 {
-			continue
-		}
-
-		c := &wire.Command{
-			Cmd:  strings.ToUpper(args[0]),
-			Args: args[1:],
-		}
-
-		resp := client.Fire(c)
-
-		if strings.HasSuffix(strings.ToUpper(args[0]), ".WATCH") {
-			fmt.Println("Entered watch mode for", c.Cmd, strings.Join(c.Args, " "))
-
-			// Create a watch-specific signal channel
-			watchSigChan := make(chan os.Signal, 1)
-			signal.Notify(watchSigChan, os.Interrupt)
-
-			ch, err := client.WatchCh()
-			if err != nil {
-				fmt.Println("Error watching:", err)
-				continue
-			}
-
-			// resp, _ := <-ch
-
-			var fingerprint string
-			if len(resp.Attrs.AsMap()) > 0 {
-				for k, v := range resp.Attrs.AsMap() {
-					if k == "fingerprint" {
-						fingerprint = v.(string) // Extract fingerprint
-					}
-				}
-			}
-
-			// Set watch mode flag
-			currentMode = WatchMode
-
-			// Goroutine to listen to watch channel
-			go func() {
-				for {
-					select {
-					case resp, ok := <-ch:
-						if !ok {
-							fmt.Println("Watch channel closed")
-							return
-						}
-						renderResponse(resp)
-					}
-				}
-			}()
-
-			// Wait until watch mode exits
-			<-watchSigChan
-			fmt.Println("Exiting watch mode...")
-    		currentMode = CommandMode
-    		signal.Stop(watchSigChan)
-
-			unwatchc := &wire.Command{
-				Cmd:  "UNWATCH",
-				Args: []string{fingerprint},
-			}
-
-			client.Fire(unwatchc)
-
-		} else {
-			renderResponse(resp)
 		}
 	}
 }
 
-func renderResponse(resp *wire.Response) {
-	if resp.Err != "" {
-		fmt.Printf("%s %s\n", boldRed("ERR"), resp.Err)
+func (c *CLI) handleInput(input string) bool {
+	if input == "exit" {
+		return false
+	}
+
+	if input == "" {
+		return true
+	}
+
+	args := parseArgs(input)
+	if len(args) == 0 {
+		return true
+	}
+
+	cmd := &wire.Command{
+		Cmd:  strings.ToUpper(args[0]),
+		Args: args[1:],
+	}
+
+	resp := c.client.Fire(cmd)
+
+	if strings.HasSuffix(strings.ToUpper(args[0]), ".WATCH") {
+		c.handleWatchMode(cmd, resp)
+	} else {
+		c.renderResponse(resp)
+	}
+
+	return true
+}
+
+func (c *CLI) handleWatchMode(cmd *wire.Command, resp *wire.Response) {
+	fmt.Printf("Entered watch mode for %s %s\n", cmd.Cmd, strings.Join(cmd.Args, " "))
+
+	signal.Notify(c.watchSigChan, os.Interrupt)
+	defer signal.Stop(c.watchSigChan)
+
+	ch, err := c.client.WatchCh()
+	if err != nil {
+		fmt.Printf("Error watching: %v\n", err)
 		return
 	}
 
-	fmt.Printf("%s ", boldGreen("OK"))
+	fingerprint := c.extractFingerprint(resp)
+	c.mode = WatchMode
+
+	done := make(chan struct{})
+	go c.watchLoop(ch, done)
+
+	<-c.watchSigChan
+	fmt.Println("\nExiting watch mode...")
+	close(done)
+	c.mode = CommandMode
+
+	unwatchCmd := &wire.Command{
+		Cmd:  "UNWATCH",
+		Args: []string{fingerprint},
+	}
+	c.client.Fire(unwatchCmd)
+}
+
+func (c *CLI) watchLoop(ch <-chan *wire.Response, done chan struct{}) {
+	for {
+		select {
+		case resp, ok := <-ch:
+			if !ok {
+				fmt.Println("Watch channel closed")
+				return
+			}
+			c.renderResponse(resp)
+		case <-done:
+			return
+		}
+	}
+}
+
+func (c *CLI) extractFingerprint(resp *wire.Response) string {
+	if attrs := resp.Attrs.AsMap(); len(attrs) > 0 {
+		if fp, ok := attrs["fingerprint"].(string); ok {
+			return fp
+		}
+	}
+	return ""
+}
+
+func (c *CLI) renderResponse(resp *wire.Response) {
+	if resp.Err != "" {
+		fmt.Printf("%s %s\n", c.boldRed("ERR"), resp.Err)
+		return
+	}
+
+	fmt.Printf("%s ", c.boldGreen("OK"))
 	if len(resp.Attrs.AsMap()) > 0 {
-		attrs := []string{}
+		attrs := make([]string, 0, len(resp.Attrs.AsMap()))
 		for k, v := range resp.Attrs.AsMap() {
 			attrs = append(attrs, fmt.Sprintf("%s=%s", k, v))
 		}
 		fmt.Printf("[%s] ", strings.Join(attrs, ", "))
 	}
 
-	switch resp.Value.(type) {
+	switch v := resp.Value.(type) {
 	case *wire.Response_VStr:
-		fmt.Printf("%s\n", resp.Value.(*wire.Response_VStr).VStr)
+		fmt.Printf("%s\n", v.VStr)
 	case *wire.Response_VInt:
-		fmt.Printf("%d\n", resp.Value.(*wire.Response_VInt).VInt)
+		fmt.Printf("%d\n", v.VInt)
 	case *wire.Response_VFloat:
-		fmt.Printf("%f\n", resp.Value.(*wire.Response_VFloat).VFloat)
+		fmt.Printf("%f\n", v.VFloat)
 	case *wire.Response_VBytes:
-		fmt.Printf("%s\n", resp.Value.(*wire.Response_VBytes).VBytes)
+		fmt.Printf("%s\n", v.VBytes)
 	case *wire.Response_VNil:
 		fmt.Printf("(nil)\n")
 	}
