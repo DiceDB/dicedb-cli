b54a1c1f89c6da603dd30f9468570550e644144d
Thedarkmatter10 <ssatyamsingh1010@gmail.com>
2025-04-17 10:38:17 +0530

    feat(cli): replace custom argument parser with google/shlex for robust CLI input handling

--- go.mod
+++ go.mod
@@ -7,14 +7,15 @@ require (
 	github.com/dicedb/dicedb-go v1.0.4
 	github.com/fatih/color v1.18.0
 	github.com/spf13/cobra v1.8.1
+	google.golang.org/protobuf v1.36.5
 )
 
 require (
+	github.com/google/shlex v0.0.0-20191202100458-e7afc7fbc510
 	github.com/google/uuid v1.6.0 // indirect
 	github.com/inconshreveable/mousetrap v1.1.0 // indirect
 	github.com/mattn/go-colorable v0.1.13 // indirect
 	github.com/mattn/go-isatty v0.0.20 // indirect
 	github.com/spf13/pflag v1.0.5 // indirect
 	golang.org/x/sys v0.25.0 // indirect
-	google.golang.org/protobuf v1.36.5 // indirect
 )
--- ironhawk/main.go
+++ ironhawk/main.go
@@ -11,6 +11,7 @@ import (
 	"github.com/dicedb/dicedb-go"
 	"github.com/dicedb/dicedb-go/wire"
 	"github.com/fatih/color"
+	"github.com/google/shlex"
 	"google.golang.org/protobuf/types/known/structpb"
 )
 
@@ -187,29 +188,10 @@ func renderResponse(resp *wire.Response) {
 }
 
 func parseArgs(input string) []string {
-	var args []string
-	var currentArg string
-	inQuotes := false
-	var quoteChar byte = '"'
-
-	for i := 0; i < len(input); i++ {
-		c := input[i]
-		if c == ' ' && !inQuotes {
-			if currentArg != "" {
-				args = append(args, currentArg)
-				currentArg = ""
-			}
-		} else if (c == '"' || c == '\'') && !inQuotes {
-			inQuotes = true
-			quoteChar = c
-		} else if c == quoteChar && inQuotes {
-			inQuotes = false
-		} else {
-			currentArg += string(c)
-		}
-	}
-	if currentArg != "" {
-		args = append(args, currentArg)
+	args, err := shlex.Split(input)
+	if err != nil {
+		fmt.Printf("%s failed to parse command: %v\n", boldRed("ERR"), err)
+		return []string{}
 	}
 	return args
 }
